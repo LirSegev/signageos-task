c16b2a2fb3fd8c782140b0ad74ddbbdfa3a82e55
Lir Segev <lir.segev@gmail.com>
2020-02-09 13:16:56 +0100

    Fetch heroes based on searchQuery
    Install shortid

--- src/features/list/List.tsx
+++ src/features/list/List.tsx
@@ -1,16 +1,49 @@
 import React from "react";
+import { connect } from "react-redux";
+import { RootState } from "../../store";
+import { fetchHeroes } from "../../utils/api";
+import { generate } from "shortid";
 
 export interface ListState {
   results: string[];
 }
+export interface ListProps {
+  searchQuery: string;
+}
 
-class List extends React.Component<{}, ListState> {
+class List extends React.Component<ListProps, ListState> {
   state: ListState = {
     results: []
   };
+
+  componentDidMount() {
+    this._updateResults();
+  }
+
+  componentDidUpdate() {
+    this._updateResults(this.props.searchQuery);
+  }
+
+  _updateResults = (searchQuery = "") => {
+    fetchHeroes(searchQuery)
+      .then(results => {
+        this.setState({
+          results
+        });
+      })
+      .catch(err => console.error(err));
+  };
+
   render() {
-    return <div>{this.state.results}</div>;
+    const listItems = this.state.results.map(result => (
+      <li key={`listItem-${generate()}`}>{result}</li>
+    ));
+
+    return <ul>{listItems}</ul>;
   }
 }
 
-export default List;
+const mapState = (state: RootState) => ({
+  searchQuery: state.list.searchQuery
+});
+export default connect(mapState)(List);
